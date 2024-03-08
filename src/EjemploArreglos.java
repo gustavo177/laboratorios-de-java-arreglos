1dbbdfdb3bbc0f24d971daaec898e6f32418917d
gustavo177 <gtvadf177@hotmail.com>
2024-03-08 10:51:04 -0500

    Ejemplo de un arreglo de string y ordenar con sort

--- src/EjemploArreglos.java
+++ src/EjemploArreglos.java
@@ -1,12 +1,44 @@
+import java.util.Arrays;
 
 public class EjemploArreglos {
     public static void main(String[] args) {
+        String[] productos = new String[7];
+        productos[0] = "Kingston Pendrive 64GB";
+        productos[1] = "Samsung Galaxy";
+        productos[2] = "Disco duro SSD Samsung Externo";
+        productos[3] = "Asus Notebook";
+        productos[4] = "Macbook Air";
+        productos[5] = "Chromecast 4ta generación";
+        productos[6] = "Bicicleta Oxford";
+
+        Arrays.sort(productos);
+
+        String prod1 = productos[0];
+        String prod2 = productos[1];
+        String prod3 = productos[2];
+        String prod4 = productos[3];
+        String prod5 = productos[4];
+        String prod6 = productos[5];
+        String prod7 = productos[6];
+
+        System.out.println("productos[0] = " + prod1);
+        System.out.println("productos[1] = " + prod2);
+        System.out.println("productos[2] = " + prod3);
+        System.out.println("productos[3] = " + prod4);
+        System.out.println("productos[4] = " + prod5);
+        System.out.println("productos[5] = " + prod6);
+        System.out.println("productos[6] = " + prod7);
+
+
+
         int[] numeros = new int[4];
         
         numeros[0]=16;
         numeros[1]=Integer.valueOf("9");
         numeros[2]=3;
         numeros[3]=-6;
+
+        Arrays.sort(numeros);
 
         int i = numeros[0];
         int j = numeros[1];
