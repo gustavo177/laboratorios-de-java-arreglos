f0315bd2d2851505fee2da702eb946bfc7026e60
gustavo177 <gtvadf177@hotmail.com>
2024-03-10 11:42:07 -0500

    Ordenamiento algoritmo burbuja

---
+++ src/EjemploArreglosForOrdenamientoBurbuja.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class EjemploArreglosForOrdenamientoBurbuja {
+    public static void arregloInverso(String[] arreglo){
+        int total2 = arreglo.length;
+        int total = arreglo.length;
+        for(int i = 0; i < total2; i++){
+            String actual = arreglo[i];
+            String inverso = arreglo[total-1-i];
+            arreglo[i] = inverso;
+            arreglo[total-1-i] = actual;
+            total2--;
+        }
+    }
+    public static void main(String[] args) {
+        // String[] productos = new String[7];
+        String[] productos = {"Kingston Pendrive 64GB", "Samsung Galaxy", "Disco duro SSD Samsung Externo", "Asus Notebook",
+                "Macbook Air", "Chromecast 4ta generación", "Bicicleta Oxford"};
+        int total = productos.length;
+
+        int contador = 0;
+
+        for (int i = 0; i < total; i++) {
+            for (int j = 0; j < total; j++) {
+                if (productos[i].compareTo(productos[j])<0){
+                    String auxiliar = productos[i];
+                    productos[i] = productos[j];
+                    productos[j] = auxiliar;
+                }
+                contador++;
+            }
+        }
+
+        //Arrays.sort(productos);
+        //arregloInverso(productos);
+
+        //Collections.reverse(Arrays.asList(productos));
+
+        System.out.println("=== Usando for ===");
+        for (int i = 0; i < total; i++) {
+            System.out.println("para indice " + i + " : " + productos[i]);
+        }
+    }
+}
