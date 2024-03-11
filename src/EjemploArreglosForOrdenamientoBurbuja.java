6b9a0c9f1a67063b86da482ab94ecb4a676f9f9e
gustavo177 <gtvadf177@hotmail.com>
2024-03-11 12:22:09 -0500

    Reutilizando algoritmo burbuja

--- src/EjemploArreglosForOrdenamientoBurbuja.java
+++ src/EjemploArreglosForOrdenamientoBurbuja.java
@@ -12,34 +12,46 @@ public class EjemploArreglosForOrdenamientoBurbuja {
             total2--;
         }
     }
+
+    public static void sortBurbuja(Object[] arreglo){
+        int total = arreglo.length;
+        int contador = 0;
+
+        for (int i = 0; i < total - 1; i++) {
+            for (int j = 0; j < total -1 -i; j++) {
+                if (((Comparable) arreglo[j+1]).compareTo(arreglo[j])<0){
+                    Object auxiliar = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = auxiliar;
+                }
+                contador++;
+            }
+        }
+        System.out.println("contador = " + contador);
+    }
     public static void main(String[] args) {
         // String[] productos = new String[7];
         String[] productos = {"Kingston Pendrive 64GB", "Samsung Galaxy", "Disco duro SSD Samsung Externo", "Asus Notebook",
                 "Macbook Air", "Chromecast 4ta generación", "Bicicleta Oxford"};
         int total = productos.length;
 
-        int contador = 0;
-
-        for (int i = 0; i < total - 1; i++) {
-            for (int j = 0; j < total -1 -i; j++) {
-                if (productos[j+1].compareTo(productos[j])<0){
-                    String auxiliar = productos[j];
-                    productos[j] = productos[j+1];
-                    productos[j+1] = auxiliar;
-                }
-                contador++;
-            }
-        }
-        System.out.println("contador = " + contador);
-
-        //Arrays.sort(productos);
-        //arregloInverso(productos);
-
-        //Collections.reverse(Arrays.asList(productos));
+        sortBurbuja(productos);
 
         System.out.println("=== Usando for ===");
         for (int i = 0; i < total; i++) {
             System.out.println("para indice " + i + " : " + productos[i]);
+        }
+
+        Integer[] numeros = new Integer[4];
+
+        numeros[0]=16;
+        numeros[1]=Integer.valueOf("9");
+        numeros[2]=3;
+        numeros[3]=-6;
+
+        sortBurbuja(numeros);
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("i = " + i + " : " +numeros[i]);
         }
     }
 }
