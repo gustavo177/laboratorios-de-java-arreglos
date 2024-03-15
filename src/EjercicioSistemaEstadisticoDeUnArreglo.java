8135efa82b2eb240eb0a272ea71f14a16c4a47a2
gustavo177 <gtvadf177@hotmail.com>
2024-03-15 11:33:30 -0500

    Sistema estadístico de un arreglo

---
+++ src/EjercicioSistemaEstadisticoDeUnArreglo.java
@@ -0,0 +1,38 @@
+/*
+* Escriba un programa que imprima el número más alto de un arreglo
+* de 7 elementos (de rango 11 a 99), por ejemplo {14, 33, 15, 36, 78, 21, 43},
+*  si se repite un valor considerar uno solo.
+* */
+import java.util.Scanner;
+
+public class EjercicioSistemaEstadisticoDeUnArreglo {
+    public static void main(String[] args) {
+        try {
+            Scanner s = new Scanner(System.in);
+            double[] numeros = new double[7];
+            double contadorDeCeros = 0;
+            double contadorDePositivos = 0;
+            double contadorDeNegativos = 0;
+            double sumaPositivo = 0;
+            double sumaNegativo = 0;
+            for (int i = 0; i < numeros.length; i++) {
+                System.out.print("Ingrese el " + i + " número: ");
+                numeros[i] = s.nextInt();
+                if (numeros[i] == 0){
+                    contadorDeCeros++;
+                } else if (numeros[i] > 0) {
+                    sumaPositivo = sumaPositivo + numeros[i];
+                    contadorDePositivos++;
+                } else {
+                    sumaNegativo = sumaNegativo - numeros[i];
+                    contadorDeNegativos++;
+                }
+            }
+            System.out.println("promedio de los números positivos = " + sumaPositivo/contadorDePositivos);
+            System.out.println("promedio de los números negativos = " + -(sumaNegativo/contadorDeNegativos));
+            System.out.println("contador de ceros = " + contadorDeCeros);
+        } catch (Exception e) {
+            System.out.println("Ingrese números enteros");
+        }
+    }
+}
